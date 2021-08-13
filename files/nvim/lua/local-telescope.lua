5d4036d5ef8bfb9010accb3ff973f3f03dffa24d
Diego Romero <jd@inkatze.com>
2021-08-12 19:53:53 -0700

    Adds telescope mappings

--- files/nvim/lua/local-telescope.lua
+++ files/nvim/lua/local-telescope.lua
@@ -1,6 +1,6 @@
-telescope = require('telescope')
-
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 map('n', '<c-p>', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<c-f>', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
