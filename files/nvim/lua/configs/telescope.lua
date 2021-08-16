da4473ab126940a00457dcc2c163f252ff5e9991
Diego Romero <jd@inkatze.com>
2021-08-16 12:29:10 -0700

    Renames configuration files

--- files/nvim/lua/configs/telescope.lua
+++ files/nvim/lua/configs/telescope.lua
@@ -3,4 +3,4 @@ local opts = { noremap = true, silent = true }
 
 map('n', '<c-p>', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<c-f>', '<cmd>Telescope live_grep<cr>', opts)
-map('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
+map('n', '<c-b>', '<cmd>Telescope buffers<cr>', opts)
