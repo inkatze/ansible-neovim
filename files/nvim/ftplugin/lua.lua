7c8e23678a48459e1f3ffc6de70045df54caa21b
Diego Romero <jd@inkatze.com>
2023-01-03 22:03:50 -0800

    Updates lua indent type

--- files/nvim/ftplugin/lua.lua
+++ files/nvim/ftplugin/lua.lua
@@ -1,4 +1,4 @@
-vim.opt_local.expandtab = false
+vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
@@ -9,51 +9,51 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 local function load_lspconfig()
-	require("lspconfig").sumneko_lua.setup({
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = "LuaJIT",
-					-- Setup your lua path
-					path = runtime_path,
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = { "vim" },
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = {
-					enable = false,
-					meh = "  ",
-				},
-			},
-		},
-	})
+  require("lspconfig").sumneko_lua.setup({
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+          -- Setup your lua path
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+          meh = "  ",
+        },
+      },
+    },
+  })
 end
 
 local function load_formatter()
-	local null_ls = require("null-ls")
+  local null_ls = require("null-ls")
 
-	null_ls.setup({
-		sources = {
-			null_ls.builtins.formatting.stylua,
-		},
-	})
+  null_ls.setup({
+    sources = {
+      null_ls.builtins.formatting.stylua,
+    },
+  })
 
-	vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+  vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 end
 
 local packer = require("inkatze.plugins.packer")
 
 if packer.is_installed("nvim-lspconfig") then
-	load_lspconfig()
+  load_lspconfig()
 end
 
 if packer.is_installed("null-ls.nvim") then
-	load_formatter()
+  load_formatter()
 end
--- files/nvim/lua/inkatze/plugins/packer.lua
+++ files/nvim/lua/inkatze/plugins/packer.lua
@@ -1,96 +1,96 @@
 local packer = {}
 
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
 function packer.is_installed(plugin)
-	if packer_plugins[plugin] and packer_plugins[plugin].loaded then
-		return true
-	else
-		return false
-	end
+  if packer_plugins[plugin] and packer_plugins[plugin].loaded then
+    return true
+  else
+    return false
+  end
 end
 
 function packer.start()
-	local packer_bootstrap = ensure_packer()
+  local packer_bootstrap = ensure_packer()
 
-	if not packer_bootstrap then
-		return false
-	end
+  if not packer_bootstrap then
+    return false
+  end
 
-	require("packer").init({
-		autoremove = true,
-	})
+  require("packer").init({
+    autoremove = true,
+  })
 
-	require("packer").startup(function(use)
-		use({
-			"catppuccin/nvim",
-			as = "catppuccin",
-			config = function()
-				require("inkatze.plugins.catppuccin")
-			end,
-		})
+  require("packer").startup(function(use)
+    use({
+      "catppuccin/nvim",
+      as = "catppuccin",
+      config = function()
+        require("inkatze.plugins.catppuccin")
+      end,
+    })
 
-		use({
-			"nvim-telescope/telescope.nvim",
-			branch = "0.1.x",
-			requires = { { "nvim-lua/plenary.nvim" } },
-			config = function()
-				require("inkatze.plugins.telescope").config()
-			end,
-		})
+    use({
+      "nvim-telescope/telescope.nvim",
+      branch = "0.1.x",
+      requires = { { "nvim-lua/plenary.nvim" } },
+      config = function()
+        require("inkatze.plugins.telescope").config()
+      end,
+    })
 
-		use({
-			"folke/which-key.nvim",
-			config = function()
-				require("inkatze.plugins.which-key")
-			end,
-		})
+    use({
+      "folke/which-key.nvim",
+      config = function()
+        require("inkatze.plugins.which-key")
+      end,
+    })
 
-		use({ -- neovim's lsp pre-configurations
-			"neovim/nvim-lspconfig",
-			config = function()
-				require("inkatze.plugins.lspconfig")
-			end,
-		})
+    use({ -- neovim's lsp pre-configurations
+      "neovim/nvim-lspconfig",
+      config = function()
+        require("inkatze.plugins.lspconfig")
+      end,
+    })
 
-		use({ -- Fancy symbol trees for syntax and others
-			"nvim-treesitter/nvim-treesitter",
-			config = function()
-				require("inkatze.plugins.nvim-treesitter")
-			end,
-			run = function()
-				vim.fn.system({ "brew", "install", "tree-sitter" })
-				local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-				ts_update()
-			end,
-		})
+    use({ -- Fancy symbol trees for syntax and others
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require("inkatze.plugins.nvim-treesitter")
+      end,
+      run = function()
+        vim.fn.system({ "brew", "install", "tree-sitter" })
+        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+        ts_update()
+      end,
+    })
 
-		use({
-			"jose-elias-alvarez/null-ls.nvim",
-			requires = { { "nvim-lua/plenary.nvim" } },
-			-- config = function()
-			-- 	require("inkatze.plugins.null-ls")
-			-- end,
-			run = "brew install stylua",
-		})
+    use({
+      "jose-elias-alvarez/null-ls.nvim",
+      requires = { { "nvim-lua/plenary.nvim" } },
+      -- config = function()
+      -- 	require("inkatze.plugins.null-ls")
+      -- end,
+      run = "brew install stylua",
+    })
 
-		use({ -- Use correct comment token
-			"tpope/vim-commentary",
-		})
+    use({ -- Use correct comment token
+      "tpope/vim-commentary",
+    })
 
-		if packer_bootstrap then
-			require("packer").sync()
-		end
-	end)
+    if packer_bootstrap then
+      require("packer").sync()
+    end
+  end)
 end
 
 return packer
