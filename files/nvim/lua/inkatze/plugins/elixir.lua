5aa0efbef1cc06c42a250a5cfec5dd3245bf9ab9
Diego Romero <jd@inkatze.com>
2023-07-13 11:30:04 -0700

    Elixir fixes

--- files/nvim/lua/inkatze/plugins/elixir.lua
+++ files/nvim/lua/inkatze/plugins/elixir.lua
@@ -6,15 +6,11 @@ M.config = function()
 
   elixir.setup({
     nextls = {
-      enable = false, -- defaults to false
-      port = 9000, -- connect via TCP with the given port. mutually exclusive with `cmd`. defaults to nil
-      cmd = "path/to/next-ls", -- path to the executable. mutually exclusive with `port`
-      version = "0.5.0", -- version of Next LS to install and use. defaults to the latest version
+      enable = true, -- defaults to false
       on_attach = require("inkatze.lspconfig").on_attach,
     },
     credo = {
       enable = true, -- defaults to true
-      port = 9000, -- connect via TCP with the given port. mutually exclusive with `cmd`. defaults to nil
       on_attach = require("inkatze.lspconfig").on_attach,
     },
     elixirls = {
